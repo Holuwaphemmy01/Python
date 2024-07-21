d052b2829f041fdf664c749018aba36dc09ca941
Holuwaphemmy01 <Jacoboluwafemi2002@gmail.com>
2024-07-22 00:53:13 +0100

    oluwafemi

--- Assignment/crackers function/equalstrings.py
+++ Assignment/crackers function/equalstrings.py
@@ -1,15 +1,9 @@
-
-
-
-
-
-def equal_strings(first_string, second_string):
-	import re
-	first_compare = re.compile('first_string')
-	if re.search(second_string, first_compare) is True:
-		return (second_string, first_compare)
-	else:
-		return print('False')
+def equal_string (first_string, second_string):
 	
+	if sorted(first_string) == sorted(second_string):
+		return print("True")
+	else:
+		return print("False")
 
-equal_strings('femi', 'femi')
+
+equal_string('femi', 'fmei')
