24e761f0a5b0bdf4fb89fcfc79a50e1d0bba679d
Holuwaphemmy01 <Jacoboluwafemi2002@gmail.com>
2024-07-25 01:11:08 +0100

    oluwafemi

--- Assignment/chapter 4/excercise6.py
+++ Assignment/chapter 4/excercise6.py
@@ -2,3 +2,4 @@ def average(num,*args):
     return sum(args) / num
 
 print(average(5, 10, 15, 20))
+print(average(95, 56, 47, 52, 21, 31, 13, 26, 78))
--- Assignment/chapter 4/excercise7.py
+++
@@ -1,4 +0,0 @@
-def date_time():
-    x = datetime.datetime.today()
-    print(x)
-
